0a417d4f002356b248ffc94c7d0bc8f598890340
Gabe Sucich <gabe.sucich@gmail.com>
2020-07-25 15:35:01 -0700

    Adding the basic server routes

--- controllers/showcontroller.js
+++ controllers/showcontroller.js
@@ -4,9 +4,38 @@ var router = express.Router()
 
 var Show = require('../models/show.js')
 
-router.post('/api/show/new', (req, res) => {
+router.post('/api/shows/new', (req, res) => {
     var new_show = req.body
-    Show.addNew()
+    Show.addNew(req.body.title, req.body.seasons, req.body.episodes, req.body.minutes, data => {
+        console.log("New show added")
+        res.end()
+    })
+})
+
+router.put('/api/shows/:id', (req, res) => {
+    var id = req.params.id
+    Show.bingeShow(id, data => {
+        console.log("Show binged!")
+        res.end()
+    })
+})
+
+router.delete('/api/shows/:id', (req, res) => {
+    var id = req.params.id
+    Show.deleteShow(id, data => {
+        console.log("Show deleted!")
+        res.end()
+    })
+})
+
+router.get('/api/shows/all', (req, res) => {
+    Show.getAllShows(data => res.json(data))
+})
+
+router.get('/', (req, res) => {
+    Show.getAllShows(data => res.render("index", {
+        shows: data
+    }))
 })
 
 module.exports = router
