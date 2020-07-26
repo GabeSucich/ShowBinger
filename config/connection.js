b25ab4dbc0583cbb8748bed5b9dd2bb597d85158
Gabe Sucich <gabe.sucich@gmail.com>
2020-07-26 14:27:34 -0700

    Debugging heroku

--- config/connection.js
+++ config/connection.js
@@ -12,6 +12,9 @@ var mysql = require("mysql")
 
 var MYSQL_URL = process.env.JAWSDB_URL || "mysql://root:password@localhost3306/netflix_db"
 
+var connection = mysql.createConnection(MYSQL_URL)
+
+
 connection.connect(err => {
     if (err) {
         console.error("error connecting: " + err.stack)
