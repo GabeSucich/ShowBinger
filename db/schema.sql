49d5b320ab334a0e10c27f7ca515e7079450f92f
Gabe Sucich <gabe.sucich@gmail.com>
2020-07-26 14:25:29 -0700

    Debugging heroku

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,13 @@
 USE eeg2g2v0xyx50cl3;
 
+CREATE TABLE shows(
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(100) NOT NULL,
+    seasons INT NOT NULL,
+    episodes_per_season INT NOT NULL,
+    minutes_per_episode INT NOT NULL,
+    runtime INT NOT NULL,
+    watched BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
+)
 
